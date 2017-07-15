3e609c76112fcc68a7623393ca1dc36d46f56a85
anisurrahim <anis_rahim@yahoo.com>
2017-07-15 14:59:02 -0400

    new added for Jenkins

---
+++ readme.txt
@@ -0,0 +1,2 @@
+bin	    index2.html      readme.txt  test1.txt
+index.html  JenkinsDemo.jar  src	 test2.txt
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,9 @@ public class HelloWorld {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Hello World "+i);
 		}
+		
+		
+		System.out.println("Thanks Jenkins and Git");
 
 	}
 
